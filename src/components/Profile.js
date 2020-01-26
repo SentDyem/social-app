38c9969c21d06443decb90ec2f9032b729216e3b
SentDyem <55694826+SentDyem@users.noreply.github.com>
2020-01-26 18:36:08 +0300

    change const name

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,7 @@ import React from "react";
 const Profile = () =>
 {
     return (
-        <div className="content">
+        <div className="profile">
             <div>
                 image
             </div>
