c9bd5d35cb69768ef8b7d2e813739681e177f25a
SentDyem <sentdyem@my.com>
2020-01-27 11:47:30 +0300

    changing folders to import js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header";
-import Menu from "./components/Menu";
-import Profile from "./components/Profile";
-import Ads from "./components/Ads";
-import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Menu from "./components/Menu/Menu";
+import Profile from "./components/Profile/Profile";
+import Ads from "./components/Ads/Ads";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
